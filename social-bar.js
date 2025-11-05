4a8cb8be3bae6dae46273d82cca4441121139cad
bernadus09 <83189388+bernadus09@users.noreply.github.com>
2025-11-05 10:33:36 +0700

    Update social-bar.js

--- social-bar.js
+++ social-bar.js
@@ -1,21 +1,8 @@
 // social-bar.js
 (function() {
-  'use strict';
-  
-  // Load social bar script
   const script = document.createElement('script');
   script.type = 'text/javascript';
   script.src = '//pl16170729.effectivegatecpm.com/b3/45/84/b345842bfb07df1db75bfcf1a898e0da.js';
   script.async = true;
-  
-  script.onload = function() {
-    console.log('Social bar loaded successfully');
-  };
-  
-  script.onerror = function() {
-    console.error('Failed to load social bar');
-  };
-  
-  // Append to head or body
-  (document.head || document.body).appendChild(script);
+  document.head.appendChild(script);
 })();
