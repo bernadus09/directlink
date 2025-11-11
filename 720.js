537a16b3f2e991046972916e5394a82c58794026
bernadus09 <83189388+bernadus09@users.noreply.github.com>
2025-11-11 10:12:46 +0700

    Create 720.js

---
+++ 720.js
@@ -0,0 +1,17 @@
+// banner.js
+var atOptions = {
+  key: '53e015dac2921baea4062727c03c716b',
+  format: 'iframe',
+  height: 90,
+  width: 728,
+  params: {}
+};
+
+(function() {
+  var s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = '//ominousplant.com/53e015dac2921baea4062727c03c716b/invoke.js';
+  var h = document.getElementsByTagName('head')[0] || document.documentElement;
+  h.appendChild(s);
+})();
