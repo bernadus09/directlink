f05ea337cb9ac9fca9de44c5b19c28356c41561c
bernadus09 <83189388+bernadus09@users.noreply.github.com>
2025-11-11 22:14:02 +0700

    Update dl.js

--- dl.js
+++ dl.js
@@ -2,10 +2,10 @@
 // CONFIG
 // ========================================
 const CONFIG = {
-  popunderUrl: '//ominousplant.com/62/52/8b/62528b16899f546dcfe3d7c652d13893.js',
-  botUrl: 'https://ominousplant.com/ncm6bswk9?key=c557fff9fe194e94c8546b8e66aa5c4c',
+  popunderUrl: '//pl15663649.effectivegatecpm.com/62/52/8b/62528b16899f546dcfe3d7c652d13893.js',
+  botUrl: 'https://www.effectivegatecpm.com/ncm6bswk9?key=c557fff9fe194e94c8546b8e66aa5c4c',
   mobileUrl: 'https://rondo.my.id',
-  desktopUrl: 'https://ominousplant.com/ncm6bswk9?key=c557fff9fe194e94c8546b8e66aa5c4c',
+  desktopUrl: 'https://www.effectivegatecpm.com/ncm6bswk9?key=c557fff9fe194e94c8546b8e66aa5c4c',
   popunderDelay: 300,
   mobileDelay: 1000,
   popunderTimeout: 2000,
